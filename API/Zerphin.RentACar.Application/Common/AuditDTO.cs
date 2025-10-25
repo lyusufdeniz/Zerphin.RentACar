ff2f594f64d158a9c4fe04a8c5ef1c177d927cdb
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-10-25 16:56:09 +0300

    add generic dtos

---
+++ API/Zerphin.RentACar.Application/Common/AuditDTO.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Zerphin.RentACar.Application.Common
+{
+    public class AuditDto
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime? UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public string? DeletedBy { get; set; }
+    }
+}
---
+++ API/Zerphin.RentACar.Application/Common/PagedResultDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Zerphin.RentACar.Application.Common;
+    public class PagedResultDTO<T>
+{
+    public required IReadOnlyList<T> Items { get; init; }
+    public int TotalCount { get; init; }
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalPages => PageSize <= 0 ? 0 : (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasPrevious => PageNumber > 1;
+    public bool HasNext => PageNumber < TotalPages;
+}
