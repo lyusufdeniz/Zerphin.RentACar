9335a32f4ac0565ae4277f752bb3233b8dfbba14
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-10-25 21:02:50 +0300

    add app services

---
+++ API/Zerphin.RentACar.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Zerphin.RentACar.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
