296c7a5c177fdb9cf5b1a42bbabe0fe19ef7cab9
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-10-25 21:02:42 +0300

    add di

--- API/Zerphin.RentACar.API/Program.cs
+++ API/Zerphin.RentACar.API/Program.cs
@@ -1,16 +1,31 @@
 using Zerphin.RentACar.API.Extensions;
+using Zerphin.RentACar.API.Filters;
+using Zerphin.RentACar.Application.Extensions;
+using Zerphin.RentACar.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddHttpContextAccessor();
+
+// CORS configuration
+builder.Services.AddCorsConfiguration();
 builder.Services.AddSwagger();
 builder.Services.AddEntityFramework(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddOptions(builder.Configuration);
+builder.Services.AddAuthorization();
+
 
 var app = builder.Build();
-
+app.UseCors("AllowAll");
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
+app.AddAuthzMiddlewares(); 
 app.UseAuthorization();
 app.MapControllers();
+
 
 app.Run();
