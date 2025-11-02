b181bdc9094fba7a6b4b0ffac40d61656ea049cc
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-11-02 23:04:40 +0300

    add new services to di

--- API/Zerphin.RentACar.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ API/Zerphin.RentACar.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -15,28 +15,24 @@ public static class ServiceCollectionExtensions
         // Interceptors
         services.AddScoped<AuditableInterceptor>();
 
-        // Repositories
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
-        services.AddScoped<IRepository<User>, Repository<User>>();
-        services.AddScoped<IRepository<Role>, Repository<Role>>();
-        services.AddScoped<IRepository<RefreshToken>, Repository<RefreshToken>>();
-        services.AddScoped<IRepository<Customer>, Repository<Customer>>();
-        services.AddScoped<IRepository<Vehicle>, Repository<Vehicle>>();
-        services.AddScoped<IRepository<Rental>, Repository<Rental>>();
-        services.AddScoped<IRepository<Payment>, Repository<Payment>>();
-        services.AddScoped<IRepository<Invoice>, Repository<Invoice>>();
-        services.AddScoped<IRepository<Insurance>, Repository<Insurance>>();
-        services.AddScoped<IRepository<Location>, Repository<Location>>();
+
+        // Specific Repositories
         services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<IRoleRepository, RoleRepository>();
+        services.AddScoped<IVehicleRepository, VehicleRepository>();
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IRentalRepository, RentalRepository>();
+        services.AddScoped<IPaymentRepository, PaymentRepository>();
+        services.AddScoped<IInvoiceRepository, InvoiceRepository>();
+        services.AddScoped<IInsuranceRepository, InsuranceRepository>();
 
         // Services
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<IPasswordService, PasswordService>();
         services.AddScoped<IUserContext, UserContext>();
-        // TODO: Add EmailService and NotificationService implementations when needed
-        // services.AddScoped<IEmailService, EmailService>();
-        // services.AddScoped<INotificationService, NotificationService>();
+        services.AddScoped<ICloudinaryService, CloudinaryService>();
+
 
         return services;
     }
