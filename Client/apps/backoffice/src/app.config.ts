ec1a7ced2ad10cccda29fef90982b56eb8bddde9
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-11-03 22:03:27 +0300

    add interceptors

--- Client/apps/backoffice/src/app.config.ts
+++ Client/apps/backoffice/src/app.config.ts
@@ -1,15 +1,28 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-provideZonelessChangeDetection
+  provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withInterceptors,
+} from '@angular/common/http';
 import { appRoutes } from './app.routes';
+import { authInterceptor } from './interceptors/auth.interceptor';
+import { exceptionInterceptor } from './interceptors/exception.interceptor';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(appRoutes),
+    provideHttpClient(
+      withInterceptorsFromDi(),
+      withInterceptors([authInterceptor, exceptionInterceptor])
+    ),
+    provideCharts(withDefaultRegisterables()),
   ],
 };
