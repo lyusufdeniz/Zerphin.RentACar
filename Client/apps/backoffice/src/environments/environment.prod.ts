52db47206bb82a17c34beb021dd4da8494dfc04b
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-11-03 21:54:56 +0300

    add

---
+++ Client/apps/backoffice/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.rentacar.com/api',
+  appName: 'RentACar Backoffice',
+  version: '1.0.0',
+};
+
+
---
+++ Client/apps/backoffice/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:6001/api',
+  appName: 'RentACar Backoffice',
+  version: '1.0.0',
+};
+
