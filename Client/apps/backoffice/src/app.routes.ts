138e30047bf912cb63331c79d93e1a3e7225edf4
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-11-03 22:03:35 +0300

    update routes

--- Client/apps/backoffice/src/app.routes.ts
+++ Client/apps/backoffice/src/app.routes.ts
@@ -1,3 +1,59 @@
 import { Route } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CarsComponent } from './pages/cars/cars.component';
+import { RentalsComponent } from './pages/rentals/rentals.component';
+import { InsurancesComponent } from './pages/insurances/insurances.component';
+import { UsersComponent } from './pages/users/users.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { authGuard } from './guards/auth.guard';
+import { loginGuard } from './guards/login.guard';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [loginGuard],
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'cars',
+        component: CarsComponent,
+      },
+      {
+        path: 'rentals',
+        component: RentalsComponent,
+      },
+      {
+        path: 'insurances',
+        component: InsurancesComponent,
+      },
+      {
+        path: 'users',
+        component: UsersComponent,
+      },
+      {
+        path: 'settings',
+        component: SettingsComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'dashboard',
+      },
+    ],
+  },
+];
