5427b525fd54e45be814228600bb4debc5e37321
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-11-03 21:35:11 +0300

    delete invoice and customer features

--- API/Zerphin.RentACar.Application/Features/Customers/DeleteCustomer/DeleteCustomerResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Zerphin.RentACar.Application.Features.Customers.DeleteCustomer;
-
-public class DeleteCustomerResponse
-{
-    public Guid Id { get; set; }
-    public string Message { get; set; } = "Customer deleted successfully.";
-}
-
-
--- API/Zerphin.RentACar.Application/Features/Customers/GetCustomerStatistics/GetCustomerStatisticsCommand.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using Zerphin.RentACar.Application.Common;
-
-namespace Zerphin.RentACar.Application.Features.Customers.GetCustomerStatistics;
-
-public class GetCustomerStatisticsCommand : IRequest<ServiceResult<GetCustomerStatisticsResponse>>
-{
-}
-
